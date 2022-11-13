b6ef6bfb868928f1b96b3df73f1f7179f2655708
Aline Hammermuller <98852797+ahammermuller@users.noreply.github.com>
2022-11-12 19:01:22 -0800

    Add files via upload

---
+++ OOP_Quiz_Game/data.py
@@ -0,0 +1,44 @@
+
+question_data = [
+    {"text": "A slug's blood is green.", "answer": "True"},
+    {"text": "The loudest animal is the African Elephant.", "answer": "False"},
+    {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
+    {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
+    {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, "
+             "you are free to take it home to eat.", "answer": "True"},
+    {"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.",
+     "answer": "False"},
+    {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
+    {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
+    {"text": "Google was originally called 'Backrub'.", "answer": "True"},
+    {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
+    {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
+    {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"},
+    {"text": "&quot;Typewriter&quot; is the longest word that can be typed using only the first row on a QWERTY "
+             "keyboard.", "answer": "True"},
+    {"text": "Coca-Cola&#039;s original colour was green.", "answer": "False"},
+    {"text": "The US emergency hotline is 911 because of the September 11th terrorist attacks.", "answer": "False"},
+    {"text": "The vapor produced by e-cigarettes is actually water.", "answer": "False"},
+    {"text": "The sum of all the numbers on a roulette wheel is 666.", "answer": "True"},
+    {"text": "There are 86400 seconds in a day.", "answer": "True"},
+
+    {"text": "The word &quot;news&quot; originates from the first letters of the 4 main directions on a compass "
+             "(North, East, West, South).", "answer": "False"},
+    {"text": "Cucumbers are usually more than 90% water.", "answer": "True"},
+    {"text": "The average woman is 5 inches 13 centimeters shorter than the average man.", "answer": "True"},
+    {"text": "Sitting for more than three hours a day can cut two years off a person&#039;s life expectancy.",
+     "answer": "True"},
+    {"text": "The French word to travel is &quot;Travail&quot;", "answer": "False"},
+    {"text": "The French word for &quot;glass&quot; is &quot;glace&quot;.", "answer": "False"},
+    {"text": "Albert Einstein had trouble with mathematics when he was in school.", "answer": "False"},
+    {"text": "Haggis is traditionally ate on Burns Night.", "answer": "True"},
+    {"text": "Francis Bacon died from a fatal case of pneumonia while he was attempting to preserve meat by stuffing "
+             "a chicken with snow.", "answer": "True"},
+    {"text": "Popcorn was invented in 1871 by Frederick W. Rueckheim in the USA where he sold the snack on the "
+             "streets of Chicago.", "answer": "False"},
+    {"text": "The British organisation CAMRA stands for The Campaign for Real Ale.", "answer": "True"},
+    {"text": "The commercial UK channel ITV stands for &quot;International Television&quot;.", "answer": "False"},
+    {"text": "The pickled gherkin was first added to hamburgers because a US health law required all "
+             "fast-food to include a source of Vitamin C.", "answer": "False"},
+    {"text": "Instant mashed potatoes were invented by Canadian Edward Asselbergs in 1962.", "answer": "True"}
+]
